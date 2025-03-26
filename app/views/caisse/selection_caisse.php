a07dc2a0132e7437e14142c26afa68c2610069d1
chekalls <146121696+chekalls@users.noreply.github.com>
2025-03-26 10:48:36 +0300

    setting method action on

--- app/views/caisse/selection_caisse.php
+++ app/views/caisse/selection_caisse.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop Homepage</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <!-- Bootstrap core CSS -->
-    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="assets/css/shop-homepage.css" rel="stylesheet">
+    <link href="/assets/css/shop-homepage.css" rel="stylesheet">
 </head>
 <body>
     <div class="container py-4">
-        <form action="" class="custom-form row g-3 align-items-end">
+        <form action="/caisse/treatment/selection_caisse" class="custom-form row g-3 align-items-end" method="post" >
             <div class="col-auto">
                 <label for="caisse" class="form-label">Select Register</label>
                 <select name="caisse" id="caisse" class="form-select">
